6d23df4a6989b731b09ae9fee95f113bf06198a4
Emin Tarhan <emintarhan@icloud.com>
2022-07-14 19:08:54 +0300

    First Commit

--- BasicCalculator/ViewController.swift
+++ BasicCalculator/ViewController.swift
@@ -8,12 +8,88 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    var result = 0
+    var uyariMesaji = "Düzgün Rakam Gir Eşşoğlu"
+  
+    @IBOutlet weak var firstNum: UITextField!
+    @IBOutlet weak var secondNum: UITextField!
+    @IBOutlet weak var resultLabel: UILabel!
+   
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
 
+    
+    @IBAction func sumButtonClicked(_ sender: Any) {
+    
+        if let firstNumberTyped = Int(firstNum.text!)   {
+        if let secondNumberTyped = Int(secondNum.text!) {
+            
+            result = firstNumberTyped + secondNumberTyped
+            resultLabel.text = String(result)
+        } 
+        } else {
+            resultLabel.text = String(uyariMesaji)
+        }
+        
+        
+        
+    }
+    
+    
+    
+
+    @IBAction func minusButtonClicked(_ sender: Any) {
+    
+        if let firstNumberTyped = Int(firstNum.text!)   {
+        if let secondNumberTyped = Int(secondNum.text!) {
+            
+            result = firstNumberTyped - secondNumberTyped
+            resultLabel.text = String(result)
+        } else {
+            resultLabel.text = String(uyariMesaji)
+        }
+        }
+    }
+    
+    
+    
+    @IBAction func mulitplyButtonClicked(_ sender: Any) {
+    
+        if let firstNumberTyped = Int(firstNum.text!)   {
+        if let secondNumberTyped = Int(secondNum.text!) {
+            
+            result = firstNumberTyped * secondNumberTyped
+            resultLabel.text = String(result)
+        } else {
+            resultLabel.text = String(uyariMesaji)
+        }
+        }
+    
+    }
+    
+    
+    @IBAction func divideButtonClicked(_ sender: Any) {
+    
+        if let firstNumberTyped = Int(firstNum.text!)   {
+        if let secondNumberTyped = Int(secondNum.text!) {
+            
+            result = firstNumberTyped / secondNumberTyped
+            resultLabel.text = String(result)
+        } else {
+            resultLabel.text = String(uyariMesaji)
+        }
+        }
+    
+    }
+    
+    
+    
+    
+    
 }
 
