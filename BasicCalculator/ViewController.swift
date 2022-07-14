ebe06285c4baa4627bc2853f7d00d15cb0170573
Emin Tarhan <emintarhan@icloud.com>
2022-07-14 17:13:20 +0300

    Initial Commit

---
+++ BasicCalculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BasicCalculator
+//
+//  Created by Emin Tarhan on 14.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
